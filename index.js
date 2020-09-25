15c8a7f1d3079a321058ea8941e9b4e3ace762a7
kourtneyhuget@hotmail.com <kourtneyhuget@hotmail.com>
2020-09-25 03:20:22 +0000

    made into promises

--- index.js
+++ index.js
@@ -28,19 +28,19 @@ const fetchISSFlyOverTimes = (exampleCoords, (error, passTimes) => {
 });
 
 
-// fetchCoordsByIP('162.245.144.188', (error, coords) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
 
-//   console.log('It worked! Returned Coords:', coords);
-// });
+  console.log('It worked! Returned Coords:', coords);
+});
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:', ip);
-// });
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned IP:', ip);
+});
